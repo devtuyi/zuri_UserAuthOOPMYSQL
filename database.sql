6dc65e50d73169d013a6ef863f5d3514446107c8
Oluwatuyi Segun <63879692+soluwatuyi@users.noreply.github.com>
2022-06-21 14:08:58 +0100

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS students (
+    id int NOT NULL AUTO_INCREMENT,
+    full_names varchar(255) NOT NULL,
+    country varchar(50) NOT NULL,
+    email varchar(255) NOT NULL,
+    gender varchar(6) NOT NULL,
+    password varchar(250) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
