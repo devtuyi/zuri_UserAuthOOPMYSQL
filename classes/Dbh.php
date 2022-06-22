83fa5a7ec2b82e6c687f905f68169137b8147325
Oluwatuyi Segun <63879692+soluwatuyi@users.noreply.github.com>
2022-06-22 15:36:13 +0100

    Update Dbh.php

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -1,16 +1,19 @@
 <?php
 session_start();
 class Dbh {
+    protected string $_hostname;
+    protected string $_username;
+    protected string $_password;
+    protected string $_database;
 
     public function __construct() {
-        return $this;
+        $this->_hostname = "localhost";
+        $this->_username = "root";
+        $this->_password = "";
+        $this->_database = "zuriphp";
     }
     public function connect() {
-        $hostname = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "zuriphp";
-        $cnx = new mysqli($hostname, $username, $password, $database);
+        $cnx = new mysqli($this->_hostname, $this->_username, $this->_password, $this->_database);
         if($cnx->connect_errno) {
             // Throw error
             return false;
