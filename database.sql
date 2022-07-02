a6900074a3002f23deb057cd36e21cd020ef12ac
Oluwatuyi Segun <63879692+soluwatuyi@users.noreply.github.com>
2022-07-02 12:48:53 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS students (
     country varchar(50) NOT NULL,
     email varchar(255) NOT NULL,
     gender varchar(6) NOT NULL,
-    password varchar(250) NOT NULL,
+    password varchar(25) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (email)
 );
